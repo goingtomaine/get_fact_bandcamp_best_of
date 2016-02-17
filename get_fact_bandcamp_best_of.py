05e4c84b8c49f55597c629983633161870071c7c
Going T. Maine <maineiswhereimgoing@gmail.com>
2016-02-16 23:37:04 -0800

    Finished output formatting.

--- get_fact_bandcamp_best_of.py
+++ get_fact_bandcamp_best_of.py
@@ -4,21 +4,59 @@ import pandas as pd
 import requests
 from tqdm import tqdm
 
-markdown_fmt = '[*{},* by {}]({}) (released {})'.format
+base_fmt = '[*{}*,]({}) by {}'.format
 
-def tag_fmt(x):
-    return '[https://bandcamp.com/tag/{}]({})'.format(x.replace(' ', '-'), x)
 
-def get_markdown(data):
-    mkdn = markdown_fmt(data['title'],
-    data['artist'],
-    data['url'],
-    str(data['release_date']))
-    if len(data['tags']) > 0:
+def album_year_mo_fmt(x):
+    return '[*{}*]({}) ({})'.format(x['title'],
+                                    x['url'],
+                                    pd.to_datetime(x['release_date']).strftime('%-m/%Y'))
+
+
+def loc_fmt(x):
+    if len(x) == 0:
+        return ''
+    return ' from [{}](https://bandcamp.com/tag/{})'.format(x, x.lower())
+
+
+def early_tag_fmt(x):
+    return '[{},](https://bandcamp.com/tag/{})'.format(x, x.replace(' ', '-').replace('/', '-'))
+
+
+def last_tag_fmt(x):
+    return '[{}](https://bandcamp.com/tag/{})'.format(x, x.replace(' ', '-').replace('/', '-'))
+
+
+def get_markdown_two(x):
+    mkdn = ', '.join(x.ix[:, ['title', 'url', 'release_date'], :].apply(
+        album_year_mo_fmt).tolist())
+
+    all_tags = sorted(set(chain(*x.common_tags.values)))
+
+    if len(all_tags) > 0:
         mkdn += ' :: '
-        mkdn += ', '.join(tag_fmt(x) for x in data['tags'])
+        mkdn += ' '.join(early_tag_fmt(x) for x in all_tags[:-1])
+        mkdn += ' ' + last_tag_fmt(all_tags[-1])
 
     return mkdn
+
+
+def get_markdown(data):
+    mkdn = base_fmt(data['title'],
+                    data['url'],
+                    data['artist'])
+
+    mkdn += loc_fmt(data['location'])
+
+    mkdn += ' ({})'.format(pd.to_datetime(data['release_date']).strftime('%Y'))
+
+    if len(data['common_tags']) > 0:
+        mkdn += ' :: '
+        mkdn += ' '.join(early_tag_fmt(x) for x in data['common_tags'][:-1])
+        mkdn += ' ' + last_tag_fmt(data['common_tags'][-1])
+
+    return mkdn
+
 
 def get_album_tuple(url):
     try:
@@ -55,10 +93,10 @@ def get_bandcamp_from_fact(fact_url):
     return pd.DataFrame.from_records(results, columns=['fact', 'url', 'title', 'artist', 'release_date', 'tags'])
 
 
-def main():
+def get_data():
 
     r = requests.get('http://www.factmag.com/tag/the-best-of-bandcamp/')
-    bs = BeautifulSoup(r.text)
+    bs = BeautifulSoup(r.text, 'lxml')
     best_of_urls = list(set(x['href'] for x in bs.find_all('a') if x['href'].find(
         'best') > -1 and x['href'].find('bandcamp') > -1 and x['href'].find('tag') < 0))
 
@@ -78,6 +116,37 @@ def main():
                   tag_series.value_counts() > 1])
     df['common_tags'] = df.tags.apply(lambda x: [y for y in x if y in tag_set])
 
-
-
     return df
+
+
+def main():
+    df = get_data()
+    # missing_df = df.ix[df.title == '', :]
+    df = df.ix[df.title != '', :]
+
+    # df.sort_values(['artist', 'release_date', 'title'],
+    #                ascending=[True, True, True], inplace=True)
+    #
+    # all_mkdn = df.apply(get_markdown, axis=1).drop_duplicates().values.tolist()
+
+    all_artists = df.artist.unique()
+    all_mkdn = []
+    for x in all_artists:
+        mkdn = '**{}** &bull; '.format(x)
+        mkdn += ', '.join(sorted(set(df.ix[df.artist ==
+                                           x, :].apply(album_year_mo_fmt, axis=1).values)))
+        all_tags = sorted(
+            set(chain(*df.ix[df.artist == x, 'common_tags'].values)))
+        if len(all_tags) > 0:
+            mkdn += ' &bull; '
+            mkdn += ' '.join(early_tag_fmt(x) for x in all_tags[:-1])
+            mkdn += ' ' + last_tag_fmt(all_tags[-1])
+
+        all_mkdn.append(mkdn)
+
+    with open('list.md', 'w') as outfile:
+        outfile.write('\n\n'.join(all_mkdn))
+
+
+if __name__ == "__main__":
+    main()
